c3b6a2d388d94e5c9bec2856e24930645e45cb22
msumit35 <msumit35@gmail.com>
2020-09-05 16:42:18 +0530

    Removed unwanted files

--- LocFlixWebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace LocFlixWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- LocFlixWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LocFlixWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
